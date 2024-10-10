b57a7bdc43514f643f6125100626df1b2175de6d
HananMustafa <hananmustafaawan@gmail.com>
2024-10-10 14:44:58 +0500

    redesigned confirm password screen

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,11 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forgot Password</title>
     <link rel="stylesheet" href="style.css">
+
+    <style>
+        .back-btn {
+            margin-top: 20px;
+            text-align: center;
+        }
+
+        /* Button Styles */
+        .button {
+            display:flex;
+            flex-direction:column;
+            padding: 10px 20px;
+            margin: 10px 0;
+            background-color: rgb(0, 177, 68);
+            /* Default button color */
+            color: white;
+            text-decoration: none;
+            border: none;
+            border-radius: 5px;
+            /* Rounded button corners */
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            /* Smooth transition for hover */
+        }
+
+        .button:hover {
+            display: flex;
+            padding: 10px 20px;
+            margin: 10px 0;
+            background-color: rgb(0, 102, 39);
+            /* Button hover color */
+            color: white;
+            text-decoration: none;
+            border: none;
+            border-radius: 5px;
+            /* Rounded button corners */
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            /* Smooth transition for hover */
+
+        }
+    </style>
 </head>
+
 <body>
 
     {{-- <div class= "formbg">
@@ -19,27 +63,33 @@
 
         <div class="container">
             <div class="left">
-              <div class="header">
-                <h2 class="animation a1">Forgot Password</h2>
-                <h4 class="animation a2">Reset your account password using email</h4>
-              </div>
-              <div class="form">
-                <input type="email" id="email" name="email" required class="form-field animation a3" placeholder="Email Address">
-                @error('email')
-                <span class="error"> {{$message}} </span>
-                @enderror
+                <div class="header">
+                    <h2 class="animation a1">Forgot Password</h2>
+                    <h4 class="animation a2">Reset your account password using email</h4>
+                </div>
+                <div class="form">
+                    <input type="email" id="email" name="email" required class="form-field animation a3"
+                        placeholder="Email Address">
+                    @error('email')
+                        <span class="error"> {{ $message }} </span>
+                    @enderror
 
-                @if (session('status'))
-                
-                    <h5 style="margin-top: 5px;">{{ session('status') }}</h5>
-            @endif
-  
-                <button class="animation a6">Reset Password</button>
-              </div>
+                    @if (session('status'))
+                        <h5 style="margin-top: 5px;">{{ session('status') }}</h5>
+                    @endif
+
+                    <button class="animation a6">Reset Password</button>
+
+                    <div class="back-btn">
+                        <a href="{{ route('login') }}" class="button">Back</a>
+                    </div>
+                </div>
             </div>
             <div type="submit" class="right"></div>
-          </div>
+        </div>
+    </form>
 
 
 </body>
+
 </html>
--- resources/views/auth/password-confirm.blade.php
+++ resources/views/auth/password-confirm.blade.php
@@ -6,104 +6,240 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirm Password</title>
     <link rel="stylesheet" href="style.css">
+
+    <style>
+        .back-btn {
+            margin-top: 20px;
+            text-align: center;
+        }
+
+        /* Button Styles */
+        .button {
+            display: flex;
+            flex-direction: column;
+            padding: 10px 20px;
+            margin: 10px 0;
+            background-color: rgb(0, 177, 68);
+            /* Default button color */
+            color: white;
+            text-decoration: none;
+            border: none;
+            border-radius: 5px;
+            /* Rounded button corners */
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            /* Smooth transition for hover */
+        }
+
+        .button:hover {
+            display: flex;
+            padding: 10px 20px;
+            margin: 10px 0;
+            background-color: rgb(0, 102, 39);
+            /* Button hover color */
+            color: white;
+            text-decoration: none;
+            border: none;
+            border-radius: 5px;
+            /* Rounded button corners */
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            /* Smooth transition for hover */
+
+        }
+
+
+
+
+
+        * {
+            box-sizing: border-box;
+        }
+
+        @import url('https://fonts.googleapis.com/css?family=Rubik:400,500&display=swap');
+
+
+        body {
+            font-family: 'Rubik', sans-serif;
+        }
+
+        .container {
+            display: flex;
+            height: 100vh;
+        }
+
+        .left {
+            flex-basis: 40%;
+            /* overflow: hidden; */
+            display: flex;
+            flex-wrap: wrap;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            animation-name: left;
+            animation-duration: 1s;
+            animation-fill-mode: both;
+            animation-delay: 1s;
+        }
+
+        .right {
+            flex-basis: 60%;
+            /* flex: 1; */
+            background-color: black;
+            transition: 1s;
+            /* background-image: url(https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80); */
+            background-image: url("../images/NerdFlow.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+
+        .centered {
+            flex-direction: column;
+        }
+
+        .header {
+            text-align: center;
+        }
+
+        .header>h2 {
+            margin: 0;
+            color: #165e00;
+        }
+
+        .header>h4 {
+            margin-top: 10px;
+            font-weight: normal;
+            font-size: 15px;
+            color: rgba(0, 0, 0, .4);
+        }
+
+        .form {
+            width: 40%;
+            max-width: 50%;
+            /*fields/button Can aquire 50% width maximum, but in normal case, they are aquiring width which is 40%*/
+            display: flex;
+            flex-direction: column;
+            /* text-align: center; */
+        }
+
+        .Section-Form {
+            width: 40%;
+            max-width: 50%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            margin: 0 auto;
+            /* Centers the entire form horizontally */
+        }
+
+
+
+
+        /* USING MEDIA QUERIES FOR RESPONSIVENESS OF FORM IN SMALLER SCREENS */
+        @media (max-width: 1200px) {
+            .form {
+                width: 70%;
+                /* Increase width for smaller screens */
+                max-width: 80%;
+            }
+        }
+
+        @media (max-width: 768px) {
+            .form {
+                width: 90%;
+                /* Make it even wider for smaller screens */
+            }
+        }
+
+        .form>p {
+            text-align: right;
+        }
+
+        .form>p>a {
+            color: #000;
+            font-size: 14px;
+        }
+
+        .form-field {
+            width: 100%;
+            height: 46px;
+            padding: 0 16px;
+            border: 2px solid #ddd;
+            border-radius: 4px;
+            font-family: 'Rubik', sans-serif;
+            outline: 0;
+            transition: .2s;
+            margin-top: 20px;
+        }
+
+        .form-field:focus {
+            border-color: #30c404;
+        }
+
+        .form>button {
+            width: 100%;
+            padding: 12px 10px;
+            border: 0;
+            background: rgb(0, 177, 68);
+            border-radius: 3px;
+            margin-top: 10px;
+            color: #fff;
+            letter-spacing: 1px;
+            font-family: 'Rubik', sans-serif;
+            cursor: pointer;
+        }
+
+
+        .form>button:hover {
+            background-color: rgb(0, 102, 39);
+        }
+
+        .error {
+            color: red;
+            font-size: 12px;
+            margin-top: 5px;
+        }
+    </style>
 </head>
-<style>
-    * {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Ubuntu, sans-serif;
-        color: #1a1f36;
-    }
 
-    body {
-        background-color: #ffffff;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        margin: 0;
-    }
-
-    .formbg {
-        width: 100%;
-        max-width: 448px;
-        padding: 48px;
-        background: #ffffff;
-        border-radius: 4px;
-        box-shadow: rgba(60, 66, 87, 0.12) 0px 7px 14px 0px, rgba(0, 0, 0, 0.12) 0px 3px 6px 0px;
-    }
-
-    .field {
-        margin-bottom: 24px;
-    }
-
-    .field label {
-        display: block;
-        margin-bottom: 8px;
-        font-weight: 600;
-        font-size: 16px;
-    }
-
-    .field input {
-        width: 100%;
-        padding: 8px 16px;
-        font-size: 16px;
-        line-height: 28px;
-        border: 1px solid rgba(60, 66, 87, 0.16);
-        border-radius: 4px;
-        outline: none;
-        background-color: #ffffff;
-        box-shadow: rgba(60, 66, 87, 0.16) 0px 0px 0px 1px;
-    }
-
-    .btn-submit {
-        width: 100%;
-        padding: 12px;
-        font-size: 16px;
-        font-weight: 600;
-        color: #ffffff;
-        background-color: #5469d4;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-        text-align: center;
-    }
-
-    .btn-submit:hover {
-        background-color: #4357ad;
-    }
-
-    .error {
-        color: red;
-    }
-</style>
 <body>
 
-    <div class= "formbg">
-        <h1>Confirm Password</h1>
+    <form method="POST" action="{{ route('password.confirm') }}">
+        @csrf
 
-        <form method="POST" action="{{ route('password.confirm') }}">
-            @csrf
+        <div class="container">
+            <div class="left">
+                <div class="header">
+                    <h2 class="animation a1">Confirm Password</h2>
+                    <h4 class="animation a2">Confirm your authenticity using password</h4>
+                </div>
+                <div class="form">
+                    <input type="password" id="password" name="password" required class="form-field animation a4"
+                        placeholder="Password">
+                    @error('password')
+                        <span class="error"> {{ $message }} </span>
+                    @enderror
 
-            <div class="field">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            </div>
 
-            <div class="field">
+                    {{-- <div class="field">
                 <a href="{{ route('password.request') }}">Forgot Password?</a>
+            </div> --}}
+                    <button class="animation a6">Confirm</button>
+
+                    <div class="back-btn">
+                        <a href="{{ route('two-factor-setup') }}" class="button">Back</a>
+                    </div>
+
+                </div>
             </div>
 
-            @error('password')
-                <span class="error"> {{ $message }} </span>
-            @enderror
+            <div type="submit" class="right"></div>
+        </div>
 
-            <div>
-                <button type="submit" class="btn-submit">Login</button>
-            </div>
-        </form>
-    </div>
+    </form>
 
 </body>
 
