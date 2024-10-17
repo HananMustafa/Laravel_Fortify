a0415508b9901ec3f33e771e185ee773890fb33f
HananMustafa <hananmustafaawan@gmail.com>
2024-10-17 15:45:08 +0500

    Redesign home screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,19 @@
 
 @section('content')
 
+<style>
+.full-image{
+    width: 100%;
+    height: 100%;
+    object-fit:cover;
+}
+</style>
+
    
     <div class="welcome-home">
         <h1>Welcome to Home</h1>
+
+        <img src="{{ asset('images/dashboardpic.jpg') }}" alt="Nerd Flow" class="full-image">
     </div>
 
   
